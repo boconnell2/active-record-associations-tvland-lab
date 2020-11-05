e64f6cda2af5c8a1b6aaabaf4df72c14fe0bd7f0
boconnell2 <bmoconnell15@gmail.com>
2020-11-05 15:15:12 -0500

    commit tree 3978f55bd7d39e71a823716386499a6d19015598

---
+++ db/migrate/20201105201126_add_to_shows_again.rb
@@ -0,0 +1,7 @@
+class AddToShowsAgain < ActiveRecord::Migration[6.0]
+  def change
+    add_column :characters, :day, :string
+    add_column :characters, :genre, :string
+    add_column :characters, :season, :string
+  end
+end
